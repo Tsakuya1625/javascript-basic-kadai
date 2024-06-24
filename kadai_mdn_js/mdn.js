2de0a8aa2ae99c4bbe3b5e659a6497deeb22dbe3
Tsakuya1625 <167065507+Tsakuya1625@users.noreply.github.com>
2024-06-24 12:37:52 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -2,10 +2,10 @@ const today = new Date();
 
 const todayDate = () => {
     const year = today.getFullYear();
-    const Month =today.getMonth();
+    const Month =today.getMonth() + 1;
     const Date =today.getDate();
 
-    console.log(year+'年'+(Month+1)+'月'+Date+'日')
+    console.log(year + '年' + Month + '月' + Date + '日');
 }
 
 todayDate();
